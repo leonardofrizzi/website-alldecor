12e287b4e470c3e656f0f7bd9afdfbf87997c7e9
leonardofrizzi <leofrizzi15@gmail.com>
2025-09-03 18:25:52 -0300

    fix: google tag manager script

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import CookieConsent from "./components/CookieConsent";
 import WhatsAppButton from "./components/WhatsAppButton";
+import Script from 'next/script'; // Added import
 
 const montserrat = Montserrat({
   subsets: ["latin"],
@@ -24,15 +25,19 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-BR">
-      {/* Google tag (gtag.js) */}
-      <script async src="https://www.googletagmanager.com/gtag/js?id=AW-17529545370"></script>
-      <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'AW-17529545370');
-      </script>
+      {/* Google Tag Manager - Global site tag (gtag.js) */}
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=AW-17529545370"
+      />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'AW-17529545370');
+        `}
+      </Script>
       <body>{children}</body>
     </html>
   )
