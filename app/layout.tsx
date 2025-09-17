6fa3f5ba26501b0c61e2282c726ee120be5b14b7
leonardofrizzi <leofrizzi15@gmail.com>
2025-09-17 10:39:38 -0300

    fix: google tag manager script

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,20 +25,22 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-BR">
-      {/* Google Tag Manager - Global site tag (gtag.js) */}
-      <Script
-        async
-        src="https://www.googletagmanager.com/gtag/js?id=AW-17529545370"
-      />
-      <Script id="google-analytics">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'AW-17529545370');
-        `}
-      </Script>
+      <head>
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-W8R6XV4M');`}
+        </Script>
+        {/* End Google Tag Manager */}
+      </head>
       <body className={`${montserrat.variable} font-sans`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W8R6XV4M"
+height="0" width="0" style={{display:"none",visibility:"hidden"}}></iframe></noscript>
+        {/* End Google Tag Manager (noscript) */}
         <Navbar />
         <main className="pt-[90px]">{children}</main>
         <Footer />
